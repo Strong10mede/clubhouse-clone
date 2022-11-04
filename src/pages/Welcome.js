347b1502b28512761e6a68fbae70ac89062adef4
Strong10mede <kmayur809@gmail.com>
2022-11-04 12:18:04 +0530

    welcome setup

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -26,7 +26,8 @@ export default function Welcome() {
           to="/invite"
           className="primaryBtn d-flex align-items-center mb-3"
         >
-          Get your username <img src="" alt="" />
+          Get your username
+          {/* <img src="" alt="" /> */}
         </Link>
         <Link>Have an invite text? Sign in</Link>
       </div>
